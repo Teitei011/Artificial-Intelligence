8ea905d6293f42805c6e70f7957b98f159efe2d1
Teitei011 <teitei@LAPTOP-US2TJQE1.localdomain>
2019-05-16 20:07:58 +0000

    Eigen Library

--- C++/ANN.hpp
+++ C++/ANN.hpp
@@ -1,10 +1,7 @@
-include <vector>;
+#include <vector>
 
 class NeuralLayer{
-  constructor(neurons_inputs, neurons){
-    std::vector<float> weights;
-
-
+  NeuralLayer(int neurons_inputs, int neurons);
 };
 
 class NeuralNetwork{
@@ -13,16 +10,14 @@ class NeuralNetwork{
   int _input_layer, _output_layer;
   int _learning_rate;
 
-  std::vector<float> _layers;
+  NeuralLayer _layers;
+
+  NeuralNetwork(int number_of_layers, int neurons, int input_layer, int output_layer, float learning_rate);
 
 public:
-  NeuralNetwork(int number_of_layers, int neurons, int input_layer, int output_layer, int learning_rate)
-    : _number_of_layers(number_of_layers), _neurons(neurons), _input_layer(input_layer),
-    _output_layer(output_layer), _learning_rate(learning_rate);
-
     void saveBrain();
     void loadBrain();
-    NeuralLayer activation();
+    std::vector<float> activation();
     std::vector<float> query();
     std::vector<float> train();
 
@@ -30,3 +25,42 @@ public:
     void feedforward();
 
 };
+
+NeuralLayer::NeuralLayer(int neurons_inputs, int neurons){
+
+}
+
+NeuralNetwork::NeuralNetwork(int number_of_layers, int neurons, int input_layer, int output_layer, float learning_rate){
+  int _number_of_layers{number_of_layers};
+  int _neurons{neurons};
+  int _input_layer{input_layer};
+  int _output_layer{output_layer};
+  float _learning_rate{learning_rate};
+
+  _layers = NeuralLayer::NeuralLayer( _number_of_layers,  _neurons );
+
+}
+
+void NeuralNetwork::saveBrain(){
+
+}
+
+std::vector<float> NeuralNetwork::activation(NeuralLayer current){
+
+}
+
+std::vector<float> NeuralNetwork::query(){
+
+}
+
+std::vector<float> NeuralNetwork::train(){
+
+}
+
+void NeuralNetwork::backpropagation(){
+
+}
+
+void NeuralNetwork::feedforward(){
+
+}
--- C++/main.cpp
+++ C++/main.cpp
@@ -1,11 +1,11 @@
-include "ANN.hpp"
+#include "ANN.hpp"
 
 int main(int argc, char const *argv[]) {
   int number_of_layers{3};
   int neurons{10};
   int input_layer{5};
   int output_layer{5};
-  int learning_rate{0.1};
+  float learning_rate{0.1};
 
   return 0;
 }
