a295d1927715e6a325c972287995aa519d37eaf9
Teitei011 <stefantcleal14@gmail.com>
2019-07-01 09:40:02 -0300

    Save and Load implemented

--- Python3/ANN-Another_Way/ANN_Multilayer.py
+++ Python3/ANN-Another_Way/ANN_Multilayer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 class NeuralLayer():
     def __init__(self, number_of_neurons, number_of_inputs_per_neuron):
@@ -9,14 +10,15 @@ class NeuralNetwork():
         self._layers = layers
 
     def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
+        return 1. / (1. + np.exp(-x))
 
     def sigmoid_derivative(self, x):
-        return x * (1 - x)
+        return x * (1. - x)
 
     def think(self, inputs):
         '''
             Forward propagation
+
         '''
         output_from_layers = np.dot(inputs, self._layers[0]._weights)
 
@@ -30,16 +32,51 @@ class NeuralNetwork():
             print("Layer {}".format(i + 1))
             print(self._layers[i]._weights)
 
-    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+    def save(self):
+            print("\n\nCreating directory...")
+            try:
+                path = os.mkdir("Weights")
+            except:
+                print("\n\nDirectory already created...")
 
+            os.chdir("Weights/")
+
+            # Creating file to know how many arrays it has
+            data = open("Number_of_layers.txt", "w")
+            data.write(str(len(self._layers)))
+            data.close()
+
+            for i in range(len(self._layers)):
+                np.save("weights[" + str(i) + "].npy", self._layers[i])
+
+            print("\n\nWeights saved\n\n")
+            os.chdir("..")
+
+    def load(self):
+        try:
+            os.chdir("Weights/")
+        except:
+            print("No data has been saved")
+            return
+
+        data = open("Number_of_layers.txt", "r")
+        number_of_layers = data.readline()
+        data.close()
+
+        # Loading all the weights
+        for i in range(int(number_of_layers)):
+            self.layers[i] = np.load("weights[" + str(i) + "].npy") # TODO: See why this isn´t working
+
+    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
         for iteration in range(number_of_training_iterations):
             output_from_layers = self.think(training_set_inputs)
 
-            layers_errors = training_set_outputs - output_from_layers[-1]
-            layers_deltas = layers_errors * self.sigmoid_derivative(output_from_layers[-1])
+            layers_errors = training_set_outputs - output_from_layers
+            layers_deltas = layers_errors * self.sigmoid_derivative(output_from_layers)
 
             layers_adjustment = training_set_inputs.T.dot(layers_deltas)
             self._layers[0]._weights += layers_adjustment
+            
 
             for i in range(len(self._layers) - 1, 1, -1):
                 layers_errors = layers_deltas.dot(self._layers[i]._weights.T)
@@ -48,3 +85,8 @@ class NeuralNetwork():
                 layers_adjustments = output_from_layers[i].T.dot(layers_delta)
 
                 self._layers[i]._weights += layers_adjustments
+
+            # print("Loss: {} - Iteration: {}".format(self.loss_function(output_from_layers, training_set_outputs), iteration + 1))
+
+    def loss_function(self, X, Y):
+        return str(np.mean(np.square(Y - X))) # mean sum squared loss
--- Python3/ANN-Another_Way/main.py
+++ Python3/ANN-Another_Way/main.py
@@ -1,40 +1,54 @@
 from ANN_Multilayer import NeuralLayer, NeuralNetwork
 import numpy as np
+import os
+
+def load_mnist(kind='train'):
+
+    """Load MNIST data from `path`"""
+    labels_path = os.path.join('%s-labels-idx1-ubyte'% kind)
+    images_path = os.path.join('%s-images-idx3-ubyte'% kind)
+
+    with open(labels_path,'rb') as lbpath:
+        labels = np.frombuffer(lbpath.read(), dtype=np.uint8,
+                               offset=8)
+
+    with open(images_path,'rb') as imgpath:
+        images = np.frombuffer(imgpath.read(), dtype=np.uint8,
+                               offset=16).reshape(len(labels), 784)
+
+    return images, labels
+
+
+images = []
+labels = []
+
+images, labels = load_mnist()
+
 #Seed the random number generator
 np.random.seed(1)
 layers = []
+
 # Create layer 1 (40 neurons, each with 3 inputs)
-layer1 = NeuralLayer(4, 3)
+layer1 = NeuralLayer(60, 784)
+layers.append(layer1)
+
+# Create layer 1 (40 neurons, each with 3 inputs)
+
+layer2 = NeuralLayer(60, 40)
 layers.append(layer1)
 
 
 # Create layer 2 (40 neurons with 40 inputs)
-layer2 = NeuralLayer(1, 4)
+layer3 = NeuralLayer(10, 60)
 layers.append(layer2)
-
-# # Create layer 2 (a single neuron with 4 inputs)
-# layer3 = NeuralLayer(1, 40)
-# layers.append(layer3)
 
 
 # Combine the layers to create a neural network
 neural_network = NeuralNetwork(layers)
 
-neural_network.show()
-
-# The training set. We have 7 examples, each consisting of 3 input values
-# and 1 output value.
-training_set_inputs = np.array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1], [0, 0, 0]])
-training_set_outputs = np.array([[0, 1, 1, 1, 1, 0, 0]]).T
+# neural_network.show()
 
 # Train the neural network using the training set.
 # Do it 60,000 times and make small adjustments each time.
-neural_network.train(training_set_inputs, training_set_outputs, 1)
-
-print("\n\nStage 2) New synaptic weights after training: ")
-neural_network.show()
-
-# Test the neural network with a new situation.
-print("\n\nStage 3) Considering a new situation [1, 1, 0] -> ?: ")
-output = neural_network.think(np.array([1, 1, 0]))
-print(output[-1])
+neural_network.train(images, labels, 1000)
+neural_network.save()
